6d499049abc07e62c9e3ccb814916c3010203652
Victor Gardrinier <v.gardrinier@gmail.com>
2018-05-07 19:50:41 +0200

    add window.onload function and 2 other functions to hide or show the adequate divs

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const startups = document.getElementById('startups');
 const index = document.getElementById('index');
 let wind = window.location
 
+// startups.addEventListener("click", () => {
+//   wind.href = "file:///Users/victorgardrinier/code/vgardrinier/fullstack-challenges/04-Front-End/01-HTML-and-CSS/04-Advanced-selectors/profile/startups.html"
+//   });
+
+// index.addEventListener("click", () =>  {
+//   wind.href = "file:///Users/victorgardrinier/code/vgardrinier/fullstack-challenges/04-Front-End/01-HTML-and-CSS/04-Advanced-selectors/profile/index.html"
+// });
+
+const one = document.getElementById("sectionone");
+const two = document.getElementById("sectiontwo");
+
+window.onload = () => {
+  two.style.display = "none";
+};
+
 startups.addEventListener("click", () => {
-  wind.href = "file:///Users/victorgardrinier/code/vgardrinier/fullstack-challenges/04-Front-End/01-HTML-and-CSS/04-Advanced-selectors/profile/startups.html"
+one.style.display = "none";
+two.style.display = "block";
   });
 
-index.addEventListener("click", () =>  {
-  wind.href = "file:///Users/victorgardrinier/code/vgardrinier/fullstack-challenges/04-Front-End/01-HTML-and-CSS/04-Advanced-selectors/profile/index.html"
-});
+index.addEventListener("click", () => {
+two.style.display = "none";
+one.style.display = "block";
+  });
