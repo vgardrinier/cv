68a0de730c2bc19fc87ca0aa3ff5a48af96cc806
Victor Gardrinier <v.gardrinier@gmail.com>
2018-05-09 19:11:21 +0200

    add notes in js file

--- index.js
+++ index.js
@@ -31,3 +31,12 @@ one.style.display = "block";
 //     document.getElementByClassName("video").style.display = 'none';
 // }
 
+
+// <!-- responsivity at 800px -->
+
+
+// <!-- #finish experience with YEP + SVIA + DEV
+// #EDUCATION
+// #sidebar with random stuff (music + sport + softwares + books) and Startups (yep, feelter, eatsplit, bbnb, fleeze) and articles (blockchain report, stanford blog, medium, front row blockchain, stanford philosophy and principles (top 10))
+// #responsitivity
+//  -->
