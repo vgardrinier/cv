185ca1ca23e27b5c19fa60a128fe37bfb8a7b05b
Victor Gardrinier <v.gardrinier@gmail.com>
2018-05-10 13:06:26 +0200

    add js intelligence to hide and show depending on the logo selcyed on the sidebar

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 
 const startups = document.getElementById('startups');
 const index = document.getElementById('index');
+const academics = document.getElementById('academics');
 let wind = window.location
 
 
 
 const one = document.getElementById("sectionone");
 const two = document.getElementById("sectiontwo");
+const three = document.getElementById("sectionthree");
 
 
 
 
 window.onload = () => {
   two.style.display = "none";
+  three.style.display = "none";
 };
 
 startups.addEventListener("click", () => {
 one.style.display = "none";
 two.style.display = "block";
+three.style.display = "none";
   });
 
 index.addEventListener("click", () => {
 two.style.display = "none";
 one.style.display = "block";
+three.style.display = "none";
   });
+
+academics.addEventListener("click", () => {
+  two.style.display = "none";
+  one.style.display = "none";
+  three.style.display = "block";
+});
 
 
 // if (window.width < 1200) {
